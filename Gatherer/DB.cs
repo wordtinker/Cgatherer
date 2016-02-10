e452c7d2aefc84a9e61081fdaaf70b2699c1937c
wordtinker <wordtinker@yandex.ru>
2016-02-10 23:15:45 +0300

    First take.

---
+++ Gatherer/DB.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace Gatherer
+{
+    class Record
+    {
+        public int ProjectId { get; set; }
+        public string Link { get; set; }
+        public int Id { get; set; }
+    }
+
+    class DB
+    {
+        // DB connection
+        private SQLiteConnection dbConn;
+
+        public DB(string dbFile)
+        {
+            string connString = string.Format("Data Source={0};", dbFile);
+            dbConn = new SQLiteConnection(connString);
+            dbConn.Open();
+            InitializeTables();
+        }
+
+        private void InitializeTables()
+        {
+            string sql = "CREATE TABLE IF NOT EXISTS articles( project INTEGER," +
+                "link TEXT PRIMARY KEY, visited BOOLEAN)";
+            using(SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void AddPAge(int projectId, string link)
+        {
+            string sql = "INSERT OR IGNORE INTO articles " +
+                "(project, link, visited) VALUES(@prj, @link, @visited)";
+            using(SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+            {
+                cmd.Parameters.AddWithValue("@prj", projectId);
+                cmd.Parameters.AddWithValue("@link", link);
+                cmd.Parameters.AddWithValue("@visited", false);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<Record> GetUnvisistedPages()
+        {
+            List<Record> links = new List<Record>();
+            string sql = "SELECT rowid, project, link FROM articles WHERE visited=0";
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+            {
+                SQLiteDataReader reader =  cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    links.Add(new Record() {
+                        Id = reader.GetInt32(0),
+                        ProjectId = reader.GetInt32(1),
+                        Link = reader.GetString(2)
+                    });
+                }
+                reader.Close();
+            }
+            return links;
+        }
+
+        public void SetVisited(string url)
+        {
+            string sql = "UPDATE articles SET visited=1 WHERE link=@link";
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, dbConn))
+            {
+                cmd.Parameters.AddWithValue("@link", url);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Gatherer/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using System.IO;
+
+namespace Gatherer
+{
+    enum SiteType
+    {
+        GUARDIAN,
+        REGISTER
+    }
+
+    abstract class Site
+    {
+        // Members
+        protected string section;
+
+        // Properties
+
+        /// <summary>
+        /// Minimum amount of words in the article to be gathered.
+        /// </summary>
+        public abstract int WordLimit { get; }
+        public abstract string Name { get; }
+        public abstract string BasePage { get; }
+        public abstract Encoding PageEncoding { get; }
+        public abstract string Language { get; }
+
+        // Constructors 
+        public Site(string section)
+        {
+            this.section = section;
+        }
+
+        // Methods
+        protected HtmlDocument GetPage(string url)
+        {
+            Console.WriteLine("Getting {0}", url);
+            HtmlWeb htmlWeb = new HtmlWeb() {
+                AutoDetectEncoding =false,
+                OverrideEncoding = PageEncoding
+            };
+            HtmlDocument htmlDocument = htmlWeb.Load(url);
+            return htmlDocument;
+        }
+
+        public abstract IEnumerable<string> GetNewArticles();
+        public abstract bool GetContent(out string content);
+    }
+
+    class Guardian : Site
+    {
+        public override int WordLimit { get { return 600; } }
+        public override string Name { get { return "Guardian"; } }
+        public override string BasePage { get { return "http://www.theguardian.com"; } }
+        public override Encoding PageEncoding { get { return Encoding.UTF8; } }
+        public override string Language { get { return "English"; } }
+
+        public Guardian(string section) : base(section) { }
+
+        public override IEnumerable<string> GetNewArticles()
+        {
+            HtmlDocument htmlDocument = GetPage(BasePage + section);
+            var newLinks = from a in htmlDocument.DocumentNode.Descendants("a")
+                           where a.Attributes.Contains("href") &&
+                                 a.Attributes.Contains("class") &&
+                                 a.Attributes["class"].Value == "fc-item__link"
+                           select a.Attributes["href"].Value;
+            return newLinks;
+        }
+
+        public override bool GetContent(out string content)
+        {
+            HtmlDocument htmlDocument = GetPage(section);
+            // Get headline
+            string header = (from div in htmlDocument.DocumentNode.Descendants("div")
+                         where div.Attributes.Contains("class") &&
+                               div.Attributes["class"].Value == "content__standfirst"
+                         select div.InnerText)
+                         .DefaultIfEmpty("")
+                         .FirstOrDefault();
+            // Get article
+            string text = (from div in htmlDocument.DocumentNode.SelectNodes("//div[contains(@class,'content__article-body')]")
+                             from p in div.Descendants("p")
+                             select p.InnerText)
+                             .Aggregate((a,b) => a + Environment.NewLine + b);
+
+            content = header + Environment.NewLine + text;
+            return content.Length >= WordLimit;
+        }
+    }
+
+    class Register : Site
+    {
+        public override int WordLimit { get { return 300; } }
+        public override string Name { get { return "Register"; } }
+        public override string BasePage { get { return "http://www.theregister.co.uk/"; } }
+        public override Encoding PageEncoding { get { return Encoding.UTF8; } }
+        public override string Language { get { return "english"; } }
+
+        public Register(string section) : base(section) { }
+
+        public override IEnumerable<string> GetNewArticles()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public override bool GetContent(out string content)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+
+    static class SiteFactory
+    {
+        public static Site CreateSite(SiteType siteType, string section)
+        {
+            Site site = null;
+            switch (siteType)
+            {
+                case SiteType.GUARDIAN:
+                    site = new Guardian(section);
+                    break;
+                case SiteType.REGISTER:
+                    site = new Register(section);
+                    break;
+                default:
+                    throw new NotImplementedException("Wrong site type");
+            }
+            return site;
+        }
+    }
+
+    class Program
+    {
+        private static List<KeyValuePair<SiteType, string>> siteList = new List<KeyValuePair<SiteType, string>>
+            {
+                new KeyValuePair<SiteType, string>(SiteType.GUARDIAN, "/uk-news"),
+                //new KeyValuePair<SiteType, string>(SiteType.REGISTER, "")
+                // TODO
+            };
+
+        private static DB database = new DB("projects.db");
+
+        private static void MainMethod()
+        {
+            // 1. Gather new links
+            foreach (var item in siteList)
+            {
+                Site site = SiteFactory.CreateSite(item.Key, item.Value);
+                foreach (string link in site.GetNewArticles())
+                {
+                    database.AddPAge((int)item.Key, link);
+                }
+            }
+
+            // 2. Rip articles from fresh links
+            foreach (Record rec in database.GetUnvisistedPages())
+            {
+                Site site = SiteFactory.CreateSite((SiteType)rec.ProjectId, rec.Link);
+                string content = null;
+                if (site.GetContent(out content))
+                {
+                    // save to file
+                    string dirName = Path.Combine(site.Language, "corpus", site.Name);
+                    string fileName = Path.Combine(dirName, rec.Id.ToString() + ".txt");
+                    Directory.CreateDirectory(dirName);
+                    File.WriteAllText(fileName, content);
+                }
+                // update DB
+                database.SetVisited(rec.Link);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                MainMethod();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
